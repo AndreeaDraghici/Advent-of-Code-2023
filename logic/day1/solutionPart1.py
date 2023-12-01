96f5920526a97639d2909fccc0fb9a6e013380d3
AndreeaDraghici <draghici108@gmail.com>
2023-12-01 11:40:12 +0200

    solution for day1 part1

---
+++ logic/day1/solutionPart1.py
@@ -0,0 +1,28 @@
+def extract_calibration_values(lines):
+    total_sum = 0
+
+    for line in lines:
+        digits = [char for char in line if char.isdigit()]
+        if len(digits) >= 1:
+            first_digit = int(digits[0])
+            last_digit = int(digits[-1])
+            combined_digit = first_digit * 10 + last_digit
+            total_sum += combined_digit
+
+    return total_sum
+
+
+if __name__ == "__main__":
+    try:
+        with open("complex_input.txt", "r") as file:
+            calibration_input = [line.strip() for line in file.readlines()]
+
+        # Calculate the sum of all calibration values
+        result = extract_calibration_values(calibration_input)
+
+        # Print the result
+        print(f"Sum of all of the calibration values is: {result}")
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
