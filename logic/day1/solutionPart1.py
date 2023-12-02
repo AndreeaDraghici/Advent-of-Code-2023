4dfd44818ffca01536f8c36704a8132904808876
AndreeaDraghici <draghici108@gmail.com>
2023-12-02 18:21:09 +0200

    refactored day1 code solution

--- logic/day1/solutionPart1.py
+++ logic/day1/solutionPart1.py
@@ -1,28 +1,45 @@
-def extract_calibration_values(lines):
-    total_sum = 0
+class CalibrationExtractor :
+    def __init__(self, file_path) :
+        self.file_path = file_path
 
-    for line in lines:
-        digits = [char for char in line if char.isdigit()]
-        if len(digits) >= 1:
-            first_digit = int(digits[0])
-            last_digit = int(digits[-1])
-            combined_digit = first_digit * 10 + last_digit
-            total_sum += combined_digit
+    @staticmethod
+    def extract_calibration_values(lines) :
+        total_sum = 0
 
-    return total_sum
+        for line in lines :
+            digits = [char for char in line if char.isdigit()]
+            if len(digits) >= 1 :
+                first_digit = int(digits[0])
+                last_digit = int(digits[-1])
+                combined_digit = first_digit * 10 + last_digit
+                total_sum += combined_digit
+
+        return total_sum
+
+    def process_calibration_file(self) :
+        try :
+            with open(self.file_path, "r") as file :
+                calibration_input = [line.strip() for line in file.readlines()]
+
+            # Calculate the sum of all calibration values
+            result = self.extract_calibration_values(calibration_input)
+
+            # Print the result
+            print(f"Sum of all of the calibration values is: {result}")
+        except FileNotFoundError :
+            print("File not found. Please check the file path.")
+        except Exception as e :
+            print(f"An error occurred: {e}")
 
 
-if __name__ == "__main__":
-    try:
-        with open("../../input/day1/part1/input.txt", "r") as file:
-            calibration_input = [line.strip() for line in file.readlines()]
+class MainDriver :
+    @staticmethod
+    def main() :
+        file_path = "../../input/day1/part1/input.txt"
+        extractor = CalibrationExtractor(file_path)
+        extractor.process_calibration_file()
 
-        # Calculate the sum of all calibration values
-        result = extract_calibration_values(calibration_input)
 
-        # Print the result
-        print(f"Sum of all of the calibration values is: {result}")
-    except FileNotFoundError:
-        print("File not found. Please check the file path.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+if __name__ == "__main__" :
+    main = MainDriver()
+    main.main()
--- logic/day1/solutionPart2.py
+++ logic/day1/solutionPart2.py
@@ -1,78 +1,83 @@
 import re
 
-def extract_calibration_values(lines, patterns_as_numbers=None):
-    # Initialize the total sum to 0
-    total_sum = 0
 
-    # Iterate over each line in the input
-    for line in lines:
-        # List to store patterns that match, ordered by their index in the line
-        patterns_that_match_ordered_by_index = []
+class CalibrationExtractor :
+    def __init__(self, file_path, patterns_as_numbers) :
+        self.file_path = file_path
+        self.patterns_as_numbers = patterns_as_numbers
 
-        # Iterate over each pattern in patterns_as_numbers
-        for pattern_as_number in patterns_as_numbers:
-            # Check if the pattern is present in the line
-            if pattern_as_number['pattern'] in line:
-                # Find all matches of the pattern in the line
-                all_matches = re.finditer(pattern_as_number['pattern'], line)
-                for match in all_matches:
-                    # Add the match details to the list
-                    patterns_that_match_ordered_by_index.append({
-                        'index': match.start(),
-                        'pattern': pattern_as_number['pattern'],
-                        'number': pattern_as_number['number']
-                    })
+    def extract_calibration_values(self, lines) :
+        total_sum = 0
 
-        # Sort the list based on the index of matches
-        patterns_that_match_ordered_by_index.sort(key=lambda x: x['index'])
+        for line in lines :
+            patterns_that_match_ordered_by_index = []
 
-        # Extract the first and last digits based on the matches
-        first_digit = patterns_that_match_ordered_by_index[0]['number']
-        last_digit = patterns_that_match_ordered_by_index[-1]['number']
+            for pattern_as_number in self.patterns_as_numbers :
+                if pattern_as_number['pattern'] in line :
+                    all_matches = re.finditer(pattern_as_number['pattern'], line)
+                    for match in all_matches :
+                        patterns_that_match_ordered_by_index.append({
+                            'index' : match.start(),
+                            'pattern' : pattern_as_number['pattern'],
+                            'number' : pattern_as_number['number']
+                        })
 
-        # Calculate the combined digit and add it to the total sum
-        combined_digit = first_digit * 10 + last_digit
-        total_sum += combined_digit
+            patterns_that_match_ordered_by_index.sort(key=lambda x : x['index'])
 
-    # Return the final sum
-    return total_sum
+            if patterns_that_match_ordered_by_index :
+                first_digit = patterns_that_match_ordered_by_index[0]['number']
+                last_digit = patterns_that_match_ordered_by_index[-1]['number']
+                combined_digit = first_digit * 10 + last_digit
+                total_sum += combined_digit
+
+        return total_sum
+
+    def process_calibration_file(self) :
+        try :
+            with open(self.file_path, "r") as file :
+                calibration_input = [line.strip() for line in file.readlines()]
+
+            result = self.extract_calibration_values(calibration_input)
+
+            print(f"Sum of all calibration values is: {result}")
+
+        except FileNotFoundError :
+            print("File not found. Please check the file path.")
+        except Exception as e :
+            print(f"An error occurred: {e}")
 
 
-if __name__ == "__main__":
-    # List of patterns and their corresponding numbers
-    patterns_as_numbers = [
-        {'pattern': 'one', 'number': 1},
-        {'pattern': '1', 'number': 1},
-        {'pattern': 'two', 'number': 2},
-        {'pattern': '2', 'number': 2},
-        {'pattern': 'three', 'number': 3},
-        {'pattern': '3', 'number': 3},
-        {'pattern': 'four', 'number': 4},
-        {'pattern': '4', 'number': 4},
-        {'pattern': 'five', 'number': 5},
-        {'pattern': '5', 'number': 5},
-        {'pattern': 'six', 'number': 6},
-        {'pattern': '6', 'number': 6},
-        {'pattern': 'seven', 'number': 7},
-        {'pattern': '7', 'number': 7},
-        {'pattern': 'eight', 'number': 8},
-        {'pattern': '8', 'number': 8},
-        {'pattern': 'nine', 'number': 9},
-        {'pattern': '9', 'number': 9},
-    ]
+class MainDriver :
+    @staticmethod
+    def main() :
+        # List of patterns and their corresponding numbers
+        patterns_as_numbers = [
+            {'pattern' : 'one', 'number' : 1},
+            {'pattern' : '1', 'number' : 1},
+            {'pattern' : 'two', 'number' : 2},
+            {'pattern' : '2', 'number' : 2},
+            {'pattern' : 'three', 'number' : 3},
+            {'pattern' : '3', 'number' : 3},
+            {'pattern' : 'four', 'number' : 4},
+            {'pattern' : '4', 'number' : 4},
+            {'pattern' : 'five', 'number' : 5},
+            {'pattern' : '5', 'number' : 5},
+            {'pattern' : 'six', 'number' : 6},
+            {'pattern' : '6', 'number' : 6},
+            {'pattern' : 'seven', 'number' : 7},
+            {'pattern' : '7', 'number' : 7},
+            {'pattern' : 'eight', 'number' : 8},
+            {'pattern' : '8', 'number' : 8},
+            {'pattern' : 'nine', 'number' : 9},
+            {'pattern' : '9', 'number' : 9},
+        ]
 
-    try:
-        # Read input from file and strip newline characters
-        with open("../../input/day1/part2/input.txt", "r") as file:
-            calibration_input = [line.strip() for line in file.readlines()]
+        file_path = "../../input/day1/part2/input.txt"
 
-        # Calculate the sum of all calibration values
-        result = extract_calibration_values(calibration_input, patterns_as_numbers)
+        extractor = CalibrationExtractor(file_path, patterns_as_numbers)
+        extractor.process_calibration_file()
 
-        # Print the result
-        print(f"Sum of all of the calibration values is: {result}")
 
-    except FileNotFoundError:
-        print("File not found. Please check the file path.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+if __name__ == "__main__" :
+    main = MainDriver()
+    main.main()
