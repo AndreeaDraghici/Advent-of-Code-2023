0963a96cf3fa5f060fdb18e4df4927c883a9a033
AndreeaDraghici <draghici108@gmail.com>
2023-12-10 00:15:08 +0200

    solution and req for day8

--- logic/day8/solution.py
+++ logic/day8/solution.py
@@ -1,24 +1,85 @@
-def navigate_network(instructions, start_node='AAA', end_node='ZZZ'):
-    # Parse the input to create a dictionary representing the network
-    network = {}
-    for line in instructions:
-        node, connections = line.split(' = ')
-        network[node] = tuple(connections.split(', '))
+class MazeSolver :
+    def __init__(self) :
+        pass
 
-    current_node = start_node
-    steps = 0
+    @staticmethod
+    def gcd(a, b) :
+        """Compute the greatest common divisor using Euclid's algorithm."""
+        while b :
+            a, b = b, a % b
+        return a
 
-    for instruction in instructions:
-        direction = instruction[0]
-        current_node = network[current_node][1] if direction == 'R' else network[current_node][0]
-        steps += 1
+    @staticmethod
+    def part1(input_lines) :
+        """Solve Part 1 of the puzzle."""
+        instructions = input_lines[0]
+        edges = {}
+        # Parse the input and build the graph
+        for line in input_lines[2 :] :
+            parts = line.split(" ")
+            node = parts[0]
+            lhs = parts[2][1 :-1]
+            rhs = parts[3][0 :-1]
+            edges[node] = [lhs, rhs]
 
-        if current_node == end_node:
-            return steps
+        ret = 0
+        curr = "AAA"
+        # Follow the instructions until reaching 'ZZZ'
+        while curr != "ZZZ" :
+            go = instructions[ret % len(instructions)]
+            idx = 0 if go == 'L' else 1
+            curr = edges[curr][idx]
+            ret += 1
+        return ret
 
-    return steps
+    @staticmethod
+    def part2(input_lines) :
+        """Solve Part 2 of the puzzle."""
+        instructions = input_lines[0]
+        edges = {}
+        # Parse the input and build the graph
+        for line in input_lines[2 :] :
+            parts = line.split(" ")
+            node = parts[0]
+            lhs = parts[2][1 :-1]
+            rhs = parts[3][0 :-1]
+            edges[node] = [lhs, rhs]
+
+        ret = 1
+        # Iterate through nodes ending with 'A' and calculate the cycle length
+        for cand in edges.keys() :
+            if cand[-1] != 'A' :
+                continue
+            length = 0
+            now = cand
+            while now[-1] != 'Z' :
+                go = instructions[length % len(instructions)]
+                idx = 0 if go == 'L' else 1
+                now = edges[now][idx]
+                length += 1
+            # Calculate the least common multiple of current cycle length and the result so far
+            ret = ret // MazeSolver.gcd(ret, length) * length
+        return ret
+
+    @staticmethod
+    def read_input(filename) :
+        """Read the input file and return a list of lines."""
+        with open(filename) as f :
+            return f.read().strip().split('\n')
+
+    def main(self) :
+        try :
+            input_of_part1 = self.read_input("../../input/day8/part1/input.txt")
+            print(f"Takes : {self.part1(input_of_part1)} steps")
+
+            input_of_part2 = self.read_input("../../input/day8/part2/input.txt")
+            print(f"Takes : {self.part2(input_of_part2)} steps before you're only on nodes that end with Z")
+        except FileNotFoundError :
+            print("File not found. Please check the file path.")
+        except Exception as e :
+            print(f"An error occurred: {e}")
+
 
 if __name__ == "__main__" :
-    instructions = "LLR"
-    steps_to_ZZZ = navigate_network(instructions)
-    print(f"Steps required to reach ZZZ: {steps_to_ZZZ}")
+    solver = MazeSolver()
+    solver.main()
